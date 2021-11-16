b931618bef6327760d3ec8c423275b801653f550
Érick De Souza Dias <erickdesouzadias072@gmail.com>
2021-11-16 10:09:21 -0300

    Resolução de exercícios
    Resolução de exercícios de 1 a 4.

---
+++ ex002.py
@@ -0,0 +1,7 @@
+''''
+Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma
+mensagem de boas-vindas. 
+'''
+
+nome = input('Qual seu nome: ')
+print(f'Seja bem vindo(a), {nome}!')
