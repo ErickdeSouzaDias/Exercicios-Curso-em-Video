8d4b3e0eb8bbd7315e06e8b503e8317c4bc91301
Érick De Souza Dias <erickdesouzadias072@gmail.com>
2022-08-28 02:09:09 -0300

    Resolução do exercício 85.

---
+++ Python/ex085.py
@@ -0,0 +1,20 @@
+"""Exercício Python 085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma
+lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem
+crescente. """
+
+pares = []
+impares = []
+valores = [pares, impares]
+
+for posicao in range(1, 8):
+    numero = float(input(f'Digite o {posicao}º número: '))
+
+    if numero % 2 == 0:
+        pares.append(numero)
+    else:
+        impares.append(numero)
+
+print(20*'=*')
+
+print(f'Os valores pares digitados foram {pares}.')
+print(f'Os valores impares digitados foram {impares}.')
